c4604e337ad8495cb630647c9f1acd4f3db0dff0
枫谷剑仙 <mapaler@163.com>
2019-06-11 16:50:31 +0800

    可以简单修改了

---
+++ universal_function.js
@@ -0,0 +1,38 @@
+﻿//仿GM_xmlhttpRequest函数v1.3
+if (typeof(GM_xmlhttpRequest) == "undefined") {
+    var GM_xmlhttpRequest = function(GM_param) {
+
+        var xhr = new XMLHttpRequest(); //创建XMLHttpRequest对象
+        xhr.open(GM_param.method, GM_param.url, true);
+        if (GM_param.responseType) xhr.responseType = GM_param.responseType;
+        if (GM_param.overrideMimeType) xhr.overrideMimeType(GM_param.overrideMimeType);
+        xhr.onreadystatechange = function() //设置回调函数
+            {
+                if (xhr.readyState === xhr.DONE) {
+                    if (xhr.status === 200 && GM_param.onload)
+                        GM_param.onload(xhr);
+                    if (xhr.status !== 200 && GM_param.onerror)
+                        GM_param.onerror(xhr);
+                }
+            }
+
+        for (var header in GM_param.headers) {
+            xhr.setRequestHeader(header, GM_param.headers[header]);
+        }
+
+        xhr.send(GM_param.data ? GM_param.data : null);
+    }
+}
+//数字补0
+function PrefixInteger(num, length) {  
+	return (Array(length).join('0') + num).slice(-length); 
+}
+//根据id返回宠物图标的坐标
+function iconPosition(monid)
+{
+    var indexInThisPage = (monid-1) % 100;
+    var ix = indexInThisPage % 10;
+    var iy = parseInt(indexInThisPage / 10);
+    var position = [(100 + 2) * ix * -1,(100 + 2) * iy * -1];
+    return position;
+}
