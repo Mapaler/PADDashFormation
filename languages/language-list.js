f9f8231f7520a7d8d2a7e1d66fe1b6878690c66f
枫谷剑仙 <mapaler@163.com>
2020-01-18 16:51:34 +0800

    将中文技能开发迁移到技能开发版

--- languages/language-list.js
+++ languages/language-list.js
@@ -19,4 +19,8 @@
         name:"한국어",i18n:"ko",searchlist:["ko","ja"],
         guideURL:"http://www.thisisgame.com/pad/info/monster/detail.php?code=$1"
     },
+    {
+        name:"中文（简体）技能开发版",i18n:"zh-dev",searchlist:["chs","ja"],
+        guideURL:"http://pad.skyozora.com/pets/$1"
+    },
 ];
