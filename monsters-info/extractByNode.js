14c13b8cadef9523cd4822ba97a5e0004479e7b8
枫谷剑仙 <mapaler@163.com>
2022-05-26 17:04:39 +0800

    临时增加供v20测试用的武器

--- monsters-info/extractByNode.js
+++ monsters-info/extractByNode.js
@@ -99,6 +99,18 @@ officialAPI.forEach(function(lang) {
 			delete card.searchFlags;
 		monCards.push(card);
 	}
+	//测试附加属性与类型的武器用
+	for (let i = 83; i <= 95; i++)
+	{
+		let testCard = new Card(oCards[0]);
+		testCard.id = monCards.length;
+		testCard.canAssist = true;
+		if (i >= 83 && i<= 90) testCard.attrs[0] = 6;
+		if (i >= 91) testCard.attrs = [i - 91, i - 91];
+		testCard.name = "v20.0 new awoken debug test ID." + i;
+		testCard.awakenings.push(49, i);
+		monCards.push(testCard);
+	}
 
 	//加入自定义的语言
 	lang.customName.forEach(function(lcode){
