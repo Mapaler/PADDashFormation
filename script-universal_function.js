e435b65ec4d4b63293f92e8d1143ab61da046236
枫谷剑仙 <mapaler@163.com>
2022-11-21 23:12:20 +0800

    整理文档

--- script-universal_function.js
+++ script-universal_function.js
@@ -378,20 +378,24 @@ function dbDelete (db, tableName, keys) {
 }
 
 function latentUseHole(latentId) {
-	switch (true) {
-		case (latentId === 12):
-		case (latentId >= 16 && latentId <= 36):
-		case (latentId >= 43 && latentId <= 45):
+	switch (latentId) {
+		case 12: case 16: case 17: case 18: case 19:
+		case 20: case 21: case 22: case 23: case 24:
+		case 25: case 26: case 27: case 28: case 29:
+		case 30: case 31: case 32: case 33: case 34:
+		case 35: case 36: case 43: case 44: case 45:
 		{
 			return 2;
 		}
-		case (latentId >= 13 && latentId <= 15):
-		case (latentId >= 37 && latentId <= 42):
-		case (latentId >= 46):
+		case 13: case 14: case 15: case 37: case 38:
+		case 39: case 40: case 41: case 42: case 46:
+		case 47:
 		{
 			return 6;
 		}
-		case (latentId < 12):
+		case  1: case  2: case  3: case  4: case  5:
+		case  6: case  7: case  8: case  9: case 10:
+		case 11: case 12:
 		default:
 		{
 			return 1;
