2bfcbaed4fe9612271962a7cd183f6d41d61ee19
枫谷剑仙 <mapaler@163.com>
2024-07-04 19:35:33 +0800

    2024年7月4日

--- service-worker.js
+++ service-worker.js
@@ -1782,6 +1782,10 @@ const cachesMap = new Map([
 		"cf604d147a719af84fdf5cc0577f8166"
 	],
 	[
+		"sound/voice/ja/padv1214.wav",
+		"23c8dd1c75e1ad429233268f4bf75ff7"
+	],
+	[
 		"sound/voice/ja/padv1215.wav",
 		"d9132764d9d2ba2137458fb641ef46a8"
 	],
@@ -6067,7 +6071,7 @@ const cachesMap = new Map([
 	],
 	[
 		"sound/voice/ja/padv2191.wav",
-		"d07c2fc9a621d4345c053f3d2187a937"
+		"143e052f3c15efaadaf7eaae277c68e7"
 	],
 	[
 		"sound/voice/ja/padv2192.wav",
@@ -12223,7 +12227,7 @@ const cachesMap = new Map([
 	],
 	[
 		"images/cards_en/CARDS_098.PNG",
-		"0465e99b0a93aac1eeb05664a16b0714"
+		"0444c61c24766d3ba6b1aa0ffda7441a"
 	],
 	[
 		"images/cards_en/CARDS_099.PNG",
@@ -12279,7 +12283,7 @@ const cachesMap = new Map([
 	],
 	[
 		"images/cards_en/CARDS_112.PNG",
-		"5296cfdc4e8dd35978c97145eabed8ba"
+		"64cecfa864c1693e8a28826ceac42854"
 	],
 	[
 		"sound/voice/en/padv001.wav",
@@ -29871,7 +29875,7 @@ const cachesMap = new Map([
 	],
 	[
 		"doc/images/bluestacks-vhd-mount-vhd-failed.webp",
-		"7941862be127cb177b86ea4cc2877134"
+		"e41f2de6990b34ae7995a5acb0e3ea0c"
 	],
 	[
 		"doc/images/bluestacks-vhd-open-disk-management.webp",
